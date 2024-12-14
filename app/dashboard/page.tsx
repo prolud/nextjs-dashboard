a1bc4b800358ef9ee892a9cd01bc72646d4e810a
Lucas Aguiar <7lucasdaniel@gmail.com>
2024-12-14 13:49:32 -0300

    capter 10

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,9 +14,11 @@ export default async function Page() {
       </h1>
 
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        
         <Suspense fallback={<CardSkeleton />}>
           <CardWrapper />
         </Suspense>
+        
       </div>
 
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
