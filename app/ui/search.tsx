821ffe3aa28e3dd849a33a2b4072b6a85b8ae94a
Lucas Aguiar <7lucasdaniel@gmail.com>
2024-12-16 08:55:40 -0300

    chapter 11

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,18 +1,46 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce'
 
-export default function Search({ placeholder }: { placeholder: string }) {
+interface SearchProps {
+  placeholder: string
+}
+export default function Search({ placeholder }: SearchProps) {
+  const searchParams = useSearchParams()
+  const pathName = usePathname()
+  const { replace } = useRouter()
+
+  const handleSearch = useDebouncedCallback(
+    (search: string) => {
+      const params = new URLSearchParams(searchParams)
+      params.set('page', '1')
+      if (search) {
+        params.set('invoicesSearch', search)
+      }
+      else {
+        params.delete('invoicesSearch')
+      }
+
+      replace(`${pathName}?${params.toString()}`)
+    }, 500
+  )
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
+
+      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+
+      <label htmlFor="search" className="sr-only">Search</label>
+
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('invoicesSearch')?.toString()}
       />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+
     </div>
   );
 }
