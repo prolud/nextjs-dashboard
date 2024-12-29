7a3fb9f81c5e3425588f4e20b061c239eed4c06a
Lucas Aguiar <7lucasdaniel@gmail.com>
2024-12-29 13:04:38 -0300

    capter 11

--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -8,11 +8,10 @@ interface Breadcrumb {
   active?: boolean;
 }
 
-export default function Breadcrumbs({
-  breadcrumbs,
-}: {
+interface BreadcrumbsProps {
   breadcrumbs: Breadcrumb[];
-}) {
+}
+export default function Breadcrumbs({ breadcrumbs }: Readonly<BreadcrumbsProps>) {
   return (
     <nav aria-label="Breadcrumb" className="mb-6 block">
       <ol className={clsx(lusitana.className, 'flex text-xl md:text-2xl')}>
--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -4,13 +4,11 @@ import InvoiceStatus from '@/app/ui/invoices/status';
 import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
 
-export default async function InvoicesTable({
-  query,
-  currentPage,
-}: {
+interface InvoicesTableProps {
   query: string;
   currentPage: number;
-}) {
+}
+export default async function InvoicesTable({ query, currentPage }: Readonly<InvoicesTableProps>) {
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
@@ -18,41 +16,46 @@ export default async function InvoicesTable({
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
-            {invoices?.map((invoice) => (
-              <div
-                key={invoice.id}
-                className="mb-2 w-full rounded-md bg-white p-4"
-              >
-                <div className="flex items-center justify-between border-b pb-4">
-                  <div>
-                    <div className="mb-2 flex items-center">
-                      <Image
-                        src={invoice.image_url}
-                        className="mr-2 rounded-full"
-                        width={28}
-                        height={28}
-                        alt={`${invoice.name}'s profile picture`}
-                      />
-                      <p>{invoice.name}</p>
+            {
+              invoices?.map(
+                (invoice) => (
+                  <div
+                    key={invoice.id}
+                    className="mb-2 w-full rounded-md bg-white p-4"
+                  >
+                    <div className="flex items-center justify-between border-b pb-4">
+                      <div>
+                        <div className="mb-2 flex items-center">
+                          <Image
+                            src={invoice.image_url}
+                            className="mr-2 rounded-full"
+                            width={28}
+                            height={28}
+                            alt={`${invoice.name}'s profile picture`}
+                          />
+                          <p>{invoice.name}</p>
+                        </div>
+                        <p className="text-sm text-gray-500">{invoice.email}</p>
+                      </div>
+                      <InvoiceStatus status={invoice.status} />
                     </div>
-                    <p className="text-sm text-gray-500">{invoice.email}</p>
+                    
+                    <div className="flex w-full items-center justify-between pt-4">
+                      <div>
+                        <p className="text-xl font-medium">
+                          {formatCurrency(invoice.amount)}
+                        </p>
+                        <p>{formatDateToLocal(invoice.date)}</p>
+                      </div>
+                      <div className="flex justify-end gap-2">
+                        <UpdateInvoice id={invoice.id} />
+                        <DeleteInvoice id={invoice.id} />
+                      </div>
+                    </div>
                   </div>
-                  <InvoiceStatus status={invoice.status} />
-                </div>
-                <div className="flex w-full items-center justify-between pt-4">
-                  <div>
-                    <p className="text-xl font-medium">
-                      {formatCurrency(invoice.amount)}
-                    </p>
-                    <p>{formatDateToLocal(invoice.date)}</p>
-                  </div>
-                  <div className="flex justify-end gap-2">
-                    <UpdateInvoice id={invoice.id} />
-                    <DeleteInvoice id={invoice.id} />
-                  </div>
-                </div>
-              </div>
-            ))}
+                )
+              )
+            }
           </div>
           <table className="hidden min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
